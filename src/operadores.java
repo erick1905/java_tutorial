777705b0a32335fd9517caf8b892d265105ab3f8
erick1905 <erickortegacha19@gmail.com>
2018-06-27 17:16:23 -0500

    version inicial

---
+++ src/operadores.java
@@ -0,0 +1,14 @@
+
+public class operadores {
+
+	public static void main(String[] args) {
+		int a=5;
+		int b;
+		b=8;
+		int c=a+b;
+		c-=1;
+		System.out.println(c);
+
+	}
+
+}
---
+++ src/xd.java
@@ -0,0 +1,14 @@
+
+public class xd {
+
+	public static void main(String[] args) {
+		byte edad;
+		edad= 15;//valor
+		byte erick;
+		erick=15;
+		int sumar;
+		sumar=edad+erick;
+		System.out.print(sumar);
+	}
+
+}
