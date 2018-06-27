83218de7c948dc1d74385fb6f5881cb386661bf6
erick1905 <erickortegacha19@gmail.com>
2018-06-27 17:56:55 -0500

    practiicas 2

---
+++ src/CentimetrosaPulgadas.java
@@ -0,0 +1,10 @@
+
+public class CentimetrosaPulgadas {
+	public static void main(String[] args) {
+		double cm=10;
+		final double pulgadas=2.54;
+		double resultado= cm/pulgadas;
+		System.out.println("en "+cm+" cm,hay estas pulgadas "+pulgadas);
+	}
+
+}
---
+++ src/division.java
@@ -0,0 +1,13 @@
+
+public class division {
+//uso de double
+	public static void main(String[] args) {
+		double a=7;
+		double b=5;
+		double c=b/a;
+		System.out.println(c);
+		
+
+	}
+
+}
